c7c41b8e71d381f7184c889eab09456fb0a7f478
fhenrique77 <felipegoleiro550@gmail.com>
2025-05-29 20:31:27 -0300

    Alteração nos códigos

--- Prototipagem/backend/servidor.py
+++ Prototipagem/backend/servidor.py
@@ -6,18 +6,24 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/dados', methods=['POST'])
+@app.route('/gps', methods=['POST'])
 def receber_dados():
     dados = request.json
 
-    with open("Prototipagem/backend/dadosRecebidos.json", "a") as f:
+    with open("dadosRecebidos.json", "a", encoding = "utf-8") as f:
         json.dump(dados, f)
         f.write("\n")  # quebra de linha entre registros
-    hora_recebimento = datetime.datetime.now().strftime("%H:%M:%S")
     
+    
+    hora_atual = datetime.datetime.now() - datetime.timedelta(hours=3)
+    hora_recebimento = hora_atual.strftime("%H:%M:%S")
+
     print(f"[{hora_recebimento}] Dados recebidos: {dados}")
     return jsonify({"status": "ok", "mensagem": "Dados recebidos com sucesso!"})
 
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"status": "ok", "mensagem": "pong"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
